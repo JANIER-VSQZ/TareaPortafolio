43d13d199fa8b70010e5385b1310ff6b2343d194
JANIER-VSQZ <vasquezjanier31@gmail.com>
2025-06-27 21:06:11 -0600

    añadi handlebars

---
+++ siteData/pages/home.js
@@ -0,0 +1,16 @@
+export default{
+    proyectos:[
+        {
+            imagenLink: 'src/imgs/scoutsPage.png',
+            titulo: 'Pagina Scouts de Honduras',
+            descripcion: 'pagina creada como parte de proyecto grupal final clase portales web 1',
+            pageLink: 'https://daglos.github.io/Grupo2_PortalesWeb/'
+        },
+        {
+            imagenLink: 'src/imgs/flipCoinPage.png',
+            titulo: 'Flip Coin',
+            descripcion: 'Pagina que gira una moneda y de forma aleatoria se obtiene cara o cruz',
+            pageLink: 'https://janier-vsqz.github.io/flipping-coin/'
+        },
+    ]
+}
